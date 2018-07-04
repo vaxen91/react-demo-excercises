b9c7f750899af3776a5ad8fa097f3c97c7f01de2
mpietropaolo <mpp.pietropaolo@gmail.com>
2018-07-04 23:38:53 +0200

    minor impl

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+// import logo from './logo.svg'
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Person from './Person/Person'
 
 class App extends Component {
-  render() {
+  render () {
     return (
-      <div className="App">
-        <header className="App-header">
+      <div className='App'>
+        {/* <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        </header> */}
+        <h1>React</h1>
+        <p> Welcome to a demo of react lets go bruv!!!! </p>
+        <button type='button' className='btn btn-primary'>Switch Name</button>
+        <Person name='Mattia' age='23' />
+        <Person name='Marta' age='28' >My Hobbies: Racing</Person>
+        <Person name='Mirko' age='22' />
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
