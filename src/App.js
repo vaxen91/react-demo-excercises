ae17fe4f737cc1132af520424c5f14289af443c2
mpietropaolo <mpp.pietropaolo@gmail.com>
2018-07-05 01:20:51 +0200

    minor

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import React, { Component } from 'react'
-// import logo from './logo.svg'
+import logo from './logo.svg'
 import './App.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
 import Person from './Person/Person'
 
 class App extends Component {
+  constructor (props) {
+    super(props)
+    this.state = { persons: [ {name: 'Mattia', age: 23}, {name: 'Marta', age: 28} ] }
+  }
+
+  switchNameHandler () {
+    console.log('CLICK')
+  }
+
   render () {
     return (
       <div className='App'>
-        {/* <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header> */}
+        <header className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
+          <h1 className='App-title'>Welcome to React</h1>
+        </header>
         <h1>React</h1>
         <p> Welcome to a demo of react lets go bruv!!!! </p>
-        <button type='button' className='btn btn-primary'>Switch Name</button>
-        <Person name='Mattia' age='23' />
-        <Person name='Marta' age='28' >My Hobbies: Racing</Person>
-        <Person name='Mirko' age='22' />
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
       </div>
     )
   }
