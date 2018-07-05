cbad55479c309ee4f07e08b931257dcd8a6a785d
Mattia Pietropaolo <mattia.pietropaolo@connexo.it>
2018-07-05 16:26:18 +0200

    minor

--- src/App.js
+++ src/App.js
@@ -7,11 +7,26 @@ class App extends Component {
   constructor (props) {
     super(props)
     this.state = { persons: [ {name: 'Mattia', age: 23}, {name: 'Marta', age: 28} ] }
+
+    this.switchNameHandler = (newName, anotherNewName) => {
+      console.log('CLICK')
+      this.setState({ persons: [
+        {name: this.state.persons[1].name, age: 23},
+        {name: this.state.persons[0].name, age: 28}],
+      changeState: 'yes'
+      })
+    }
+
+    this.nameChangeHandler = (event) => {
+      this.setState({ persons: [
+        {name: this.state.persons[0].name, age: 23},
+        {name: event.target.value, age: 28}],
+      changeState: 'yes'
+      })
+    }
   }
 
-  switchNameHandler () {
-    console.log('CLICK')
-  }
+  // this.switchNameHandler.bind(this, this.state.persons[1].name, this.state.persons[0].name)
 
   render () {
     return (
@@ -22,9 +37,19 @@ class App extends Component {
         </header>
         <h1>React</h1>
         <p> Welcome to a demo of react lets go bruv!!!! </p>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
+        <button onClick={() => this.switchNameHandler()}>
+          Switch Name
+        </button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+        />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+          click={this.switchNameHandler}
+          change={this.nameChangeHandler}
+        />
       </div>
     )
   }
