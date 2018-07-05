b59f0ea59aa0ae580339bf9a12d3a8512f53422b
Mattia Pietropaolo <mattia.pietropaolo@connexo.it>
2018-07-05 17:59:45 +0200

    impl if condition

--- src/App.js
+++ src/App.js
@@ -6,29 +6,49 @@ import Person from './Person/Person'
 class App extends Component {
   constructor (props) {
     super(props)
-    this.state = { persons: [ {name: 'Mattia', age: 23}, {name: 'Marta', age: 28} ] }
-
+    this.state = {
+      persons: [
+        {name: 'Mattia', age: 23},
+        {name: 'Marta', age: 28}
+      ],
+      showPersons: false
+    }
     this.switchNameHandler = (newName, anotherNewName) => {
       console.log('CLICK')
       this.setState({ persons: [
         {name: this.state.persons[1].name, age: 23},
-        {name: this.state.persons[0].name, age: 28}],
-      changeState: 'yes'
+        {name: this.state.persons[0].name, age: 28}]
       })
     }
 
     this.nameChangeHandler = (event) => {
       this.setState({ persons: [
         {name: this.state.persons[0].name, age: 23},
-        {name: event.target.value, age: 28}],
-      changeState: 'yes'
+        {name: event.target.value, age: 28}]
       })
+    }
+
+    this.togglePersonHandler = () => {
+      const doesShow = this.state.showPersons
+      this.setState({ showPersons: !doesShow })
     }
   }
 
   // this.switchNameHandler.bind(this, this.state.persons[1].name, this.state.persons[0].name)
-
   render () {
+    let persons = null
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          {this.state.persons.map(person => {
+            return <Person
+              name={person.name}
+              age={person.age}
+            />
+          })}
+        </div>
+      )
+    }
     return (
       <div className='App'>
         <header className='App-header'>
@@ -37,19 +57,10 @@ class App extends Component {
         </header>
         <h1>React</h1>
         <p> Welcome to a demo of react lets go bruv!!!! </p>
-        <button onClick={() => this.switchNameHandler()}>
-          Switch Name
+        <button onClick={this.togglePersonHandler}>
+          Show Persons
         </button>
-        <Person
-          name={this.state.persons[0].name}
-          age={this.state.persons[0].age}
-        />
-        <Person
-          name={this.state.persons[1].name}
-          age={this.state.persons[1].age}
-          click={this.switchNameHandler}
-          change={this.nameChangeHandler}
-        />
+        {persons}
       </div>
     )
   }
